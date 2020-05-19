8f6bf78aae7821c3e6efe3cbf298b93ee2b24f7f
Alex <grok88@tut.by>
2020-05-19 21:29:14 +0300

    lesson31-React.createRef

--- src/components/profile/my-posts/myPosts.tsx
+++ src/components/profile/my-posts/myPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {RefObject, KeyboardEvent} from 'react';
 import style from './myPosts.module.css';
 import Post from "./post/post";
 
@@ -12,15 +12,34 @@ type PropsType = {
 }
 
 const MyPosts = (props: PropsType) => {
+
+    let newPostElement: RefObject<HTMLTextAreaElement> = React.createRef();
+
+    const addPostHandler = () => {
+        let value = newPostElement.current;
+
+        if (value) {
+            let text = value.value;
+            console.log(text)
+            value.value = ''
+        }
+    }
+
+    const textareaAddPostHandler = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.charCode === 13) {
+            addPostHandler();
+        }
+    }
+
     return (
         <div className={style.postsBlock}>
             My Posts
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement} onKeyPress={textareaAddPostHandler}></textarea>
                 </div>
                 <div>
-                    <button type='button'>Send</button>
+                    <button type='button' onClick={addPostHandler}>Send</button>
                 </div>
             </div>
             <div className={style.posts}>
