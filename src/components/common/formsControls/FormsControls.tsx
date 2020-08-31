dcdd28f2f7bacc1f2f7dbf72d9b15cb1ac7d296a
Alex <grok88@tut.by>
2020-08-31 16:56:15 +0300

    77 - redux-form add HOC

--- src/components/common/formsControls/FormsControls.tsx
+++ src/components/common/formsControls/FormsControls.tsx
@@ -1,25 +1,39 @@
 import React from "react";
 import styles from "./FormsControls.module.css";
 
-export const Textarea = ({input, meta, ...props}: any) => {
+export const Element = (Element: any) => ({input, meta, ...props}: any) => {
     const hasError = meta.touched && meta.error;
     return (
         <div className={`${styles.formControl} ${hasError ? styles.error : ''}`}>
             <div>
-                <textarea {...input} {...props}/>
+                <Element {...input} {...props}/>
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
-export const Input = ({input, meta, ...props}: any) => {
-    const hasError = meta.touched && meta.error;
-    return (
-        <div className={`${styles.formControl} ${hasError ? styles.error : ''}`}>
-            <div>
-                <input {...input} {...props}/>
-            </div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    )
-}
+export const Textarea = Element('textarea');
+export const Input = Element('input');
+
+// export const Textarea = ({input, meta, ...props}: any) => {
+//     const hasError = meta.touched && meta.error;
+//     return (
+//         <div className={`${styles.formControl} ${hasError ? styles.error : ''}`}>
+//             <div>
+//                 <textarea {...input} {...props}/>
+//             </div>
+//             {hasError && <span>{meta.error}</span>}
+//         </div>
+//     )
+// }
+// export const Input = ({input, meta, ...props}: any) => {
+//     const hasError = meta.touched && meta.error;
+//     return (
+//         <div className={`${styles.formControl} ${hasError ? styles.error : ''}`}>
+//             <div>
+//                 <input {...input} {...props}/>
+//             </div>
+//             {hasError && <span>{meta.error}</span>}
+//         </div>
+//     )
+// }
