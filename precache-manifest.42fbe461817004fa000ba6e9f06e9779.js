df1c3286e03cd6e8be2e5b796fef51406bd9a01a
Alex <grok88@tut.by>
2020-10-05 09:23:43 +0300

    Updates

---
+++ precache-manifest.42fbe461817004fa000ba6e9f06e9779.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec201e655e39746b9f2d06915d635d8",
+    "url": "/socialWeb/index.html"
+  },
+  {
+    "revision": "98b9e07adc257da04d80",
+    "url": "/socialWeb/static/css/3.36ddfcd5.chunk.css"
+  },
+  {
+    "revision": "32729a59ec4418848cc8",
+    "url": "/socialWeb/static/css/4.aa75bef1.chunk.css"
+  },
+  {
+    "revision": "6dcfed5f3e5491ec430f",
+    "url": "/socialWeb/static/css/main.534176f9.chunk.css"
+  },
+  {
+    "revision": "149c2f5635b35c56f577",
+    "url": "/socialWeb/static/js/2.154e0457.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socialWeb/static/js/2.154e0457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b9e07adc257da04d80",
+    "url": "/socialWeb/static/js/3.ae7d2490.chunk.js"
+  },
+  {
+    "revision": "32729a59ec4418848cc8",
+    "url": "/socialWeb/static/js/4.5478f842.chunk.js"
+  },
+  {
+    "revision": "6dcfed5f3e5491ec430f",
+    "url": "/socialWeb/static/js/main.cf48d63f.chunk.js"
+  },
+  {
+    "revision": "328d71f2b079e259d7fd",
+    "url": "/socialWeb/static/js/runtime-main.a8739167.js"
+  },
+  {
+    "revision": "97a730952e98c6a3f40c1e94e579b2be",
+    "url": "/socialWeb/static/media/green.97a73095.png"
+  },
+  {
+    "revision": "9e9dc538f79790079c4ef358084f0ad9",
+    "url": "/socialWeb/static/media/oval.9e9dc538.svg"
+  }
+]);
